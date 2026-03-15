7bf99031059abda6dde2ff798b84b3cbe6004eb9
seto <87982344+sertcozdemir@users.noreply.github.com>
2026-03-15 17:51:39 +0300

    test failed product service

--- dispatcher/test_dispatcher.py
+++ dispatcher/test_dispatcher.py
@@ -17,3 +17,17 @@ def test_route_users_to_user_service(monkeypatch):
     res=client.get("/users/2")
     assert res.status_code==200
     assert res.json()["id"]==2
+def test_route_products_to_product_service(monkeypatch):
+    def fake_forward(method,url, headers=None,json=None):
+        class FakeResp:
+            status_code=200
+            text='{"id":5,"name": "Keyboard", "price":1200}'
+        return FakeResp()
+    monkeypatch.setattr(dispatcher_app,"forward_request",fake_forward)
+    res=client.get("/products/5")
+    assert res.status_code == 200
+    assert res.json()["id"]==5
+    assert res.json()["name"] == "Keyboard"
+    
+
+
